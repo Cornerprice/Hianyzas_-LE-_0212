491fcc9620cabe8bfa404d746597ee3a231ae2af
Eszter Láng <lang.eszter@blathy.info>
2024-02-12 12:18:03 +0100

    Datastructure created and file read

--- Hianyzas_[LE]_0212/Program.cs
+++ Hianyzas_[LE]_0212/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] lines = File.ReadAllLines("szeptember.csv");
+Hianyzas[] data = new Hianyzas[lines.Length];
+for (int i = 1; i < lines.Length; i++)
+{
+    data[i] = new Hianyzas(lines[i]);
+}
+
+struct Hianyzas
+{
+    public string Nev;
+    public string Osztaly;
+    public int ElsoNap;
+    public int UtolsoNap;
+    public int MulasztottOrak;
+
+    public Hianyzas(string line) 
+    {
+        var splitted = line.Split(';');
+        this.Nev = splitted[0];
+        this.Osztaly = splitted[1];
+        this.ElsoNap = int.Parse(splitted[2]);
+        this.UtolsoNap = int.Parse(splitted[3]);
+        this.MulasztottOrak = int.Parse(splitted[4]);
+    }
+}
